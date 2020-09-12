a365bec398d5164e1713ff9f6ff79fe03643a5cc
Cataykus <cataykus@gmail.com>
2020-09-12 17:50:43 +0300

    BugFix

--- Assets/Scripts/UI/OutfitSelector.cs
+++ Assets/Scripts/UI/OutfitSelector.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class OutfitSelector : MonoBehaviour
 {
     [SerializeField] private Material _outfit;
+    [SerializeField] private Transform _outfitStand;
+    [SerializeField] private bool _isStandart;
 
     private Button _button;
+    private bool _isSelected = false;
 
     private void Awake()
     {
@@ -18,15 +21,48 @@ public class OutfitSelector : MonoBehaviour
     private void OnEnable()
     {
         _button.onClick.AddListener(SelectOutfit);
+
+        if (_isStandart)
+        {
+            SelectOutfit();
+        }
     }
 
     private void OnDisable()
     {
         _button.onClick.RemoveListener(SelectOutfit);
+        StopAllCoroutines();
     }
 
     private void SelectOutfit()
     {
+        OutfitSelector[] selectors = FindObjectsOfType<OutfitSelector>();
+
+        foreach (var item in selectors)
+        {
+            item.Deselect();
+        }
+
+        _isSelected = true;
         UserOutfit.Instance().SetOutfit(_outfit);
+        StartCoroutine(RotateAnimation());
+    }
+
+    public void Deselect()
+    {
+        StopAllCoroutines();
+        _isSelected = false;
+        _outfitStand.rotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    private IEnumerator RotateAnimation()
+    {
+        var wait = new WaitForEndOfFrame();
+
+        while (true)
+        {
+            _outfitStand.Rotate(Vector3.up * 50 * Time.deltaTime);
+            yield return wait;
+        }
     }
 }
